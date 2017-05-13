16c93596b75f1d724035ef46045e1dfe914b4a2b
v <v@f>
2017-05-13 15:18:43 +0200

    Blatt03Aufg03: Eine Datei pro Klasse
    Und Kommentare besser platziert.

--- Blatt03/Aufg03/Aufgabe3.java
+++ Blatt03/Aufg03/Aufgabe3.java
@@ -1,27 +1,3 @@
-class C1 {
-    static int s = 1;
-
-    void f1(){
-        System.out.println("C1::f1");
-    }
-
-    void f2(){
-        System.out.println("C1::f2");
-    }
-}
-
-class C2 extends C1 {
-    static int s = 2;
-
-    void f1(){
-        System.out.println("C2::f1");
-    }
-
-    void f3(){
-        System.out.println("C2::f3");
-    }
-}
-
 class Aufgabe3 {
     public static void main (String[] args) {
         C1 a = new C2();
@@ -36,14 +12,11 @@ class Aufgabe3 {
         b.f2();                            // "C1::f1" vererbte Methode von C1
         b.f3();                            // "C2::f3"
 
-        //C1 c = new C1(); c.f3();         // Kompilierfehler, f3()
-                                           // ist nur für die
-                                           // Unterklasse C2 definiert
+        // Kompilierfehler, f3() ist nur für die Unterklasse C2 definiert
+        // C1 c = new C1(); c.f3();
 
-        C1 c = new C1(); ((C2) c).f1();    // Laufzeitfehler, da c
-                                           // Instanz der Klasse C1
-                                           // und ein Cast auf C2
-                                           // (eine Unterklasse von
-                                           // C1) nicht möglich ist
+        // Laufzeitfehler, da c Instanz der Klasse C1 und ein Cast auf
+        // C2 (eine Unterklasse von C1) nicht möglich ist
+        C1 c = new C1(); ((C2) c).f1();
     }
 }
