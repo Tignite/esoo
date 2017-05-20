457bd20c8c23f7f5a9bd27ae2684e4758a9dc0f7
v <v@f>
2017-05-20 21:18:45 +0200

    b04a03: Kommentare hinzugefügt

--- Blatt04/Aufg03/Aufgabe3.java
+++ Blatt04/Aufg03/Aufgabe3.java
@@ -5,12 +5,18 @@ class Aufgabe3
         Student student = new Student();
 
         Cauliflower essen1 = new Cauliflower();
-        student.eat(essen1);
+        student.eat(essen1); // I don't like Blumenkohl an Gorgonzolasoße
+	// Statischer Typ und dynamischer Typ von essen1 sind hier gleich!
 
         Spaghetti essen2 = new Spaghetti();
-        student.eat(essen2);
+        student.eat(essen2); // I like Spaghetti Bologneser Art
+	// Statischer Typ und dynamischer Typ von essen2 sind hier gleich!
 
         Food essen = new Cauliflower();
-        student.eat(essen);
+        student.eat(essen); // I like Blumenkohl an Gorgonzolasoße
+	// Statischer Typ und dynamischer Typ von essen sind hier
+	// nicht gleich. Somit bestimmt der Statische Typ was
+	// "sichtbar" ist. Somit wird hier die Student.eat(Food food)
+	// Methode aufgerufen.
     }
 }
