18ff5bf9c2ee317d35cbdaa9a5eccdb93b137719
v <v@f>
2017-06-03 19:20:22 +0200

    b03a03: Whitespace and Coding System

--- Blatt05/Aufg3/Catalogue.java
+++ Blatt05/Aufg3/Catalogue.java
@@ -1,41 +1,34 @@
 import java.util.HashSet;
 import java.util.Iterator;
 
-
 public class Catalogue {
-	
-	private HashSet<Product> set = new HashSet<Product>();
-	
-	public void printMe(){
-		Iterator iterator = set.iterator();
-		String output = "Catalogue: \n";
-	
-		while(iterator.hasNext()){
-			output += iterator.next().toString() + "\n";
-		}
-		System.out.print(output);
-	}
-	
-	public void add(Product p){
-		set.add(p);
-		
-	}
-	
-	public static void main (String [] args) {
-		
-		
-		Catalogue cat = new Catalogue();
-		Product p1 = new Product("Papier", "A4", 2.52);
-		Clothing c1 = new Clothing ("Hemd", "Herren-Hemd", 33.61, "Baumwolle", 48);
-		
-		cat.add(p1);
-		cat.add(c1);
-		
-		cat.printMe();
-		
-	
-	}
-	
 
+    private HashSet<Product> set = new HashSet<Product>();
 
+    public void printMe() {
+        Iterator iterator = set.iterator();
+        String output = "Catalogue: \n";
+
+        while(iterator.hasNext()){
+            output += iterator.next().toString() + "\n";
+        }
+
+        System.out.print(output);
+    }
+
+    public void add(Product p){
+        set.add(p);
+    }
+
+    public static void main (String [] args) {
+
+        Catalogue cat = new Catalogue();
+        Product p1 = new Product("Papier", "A4", 2.52);
+        Clothing c1 = new Clothing ("Hemd", "Herren-Hemd", 33.61, "Baumwolle", 48);
+
+        cat.add(p1);
+        cat.add(c1);
+
+        cat.printMe();
+    }
 }
--- Blatt05/Aufg3/Clothing.java
+++ Blatt05/Aufg3/Clothing.java
@@ -1,19 +1,18 @@
 
 public class Clothing extends Product {
-	 
-	public String material;
-	public int size;
-	
-	public Clothing(String name, String description, double price, String material, int size){
-		super(name, description, price);
-		this.material = material;
-		this.size = size;
-	}
-	
-	//Teil b.)
-	public String toString() {
-		return name + " - " + description + " - " + price + " EUR"+ " - " + material + " - " +"size: "+ size;
-		}
-	
 
+    public String material;
+    public int size;
+
+    public Clothing(String name, String description, double price, String material, int size){
+        super(name, description, price);
+        this.material = material;
+        this.size = size;
+    }
+
+    //Teil b.)
+    public String toString() {
+        return name + " - " + description + " - "+ price
+            + " EUR"+ " - " + material + " - " +"size: "+ size;
+    }
 }
