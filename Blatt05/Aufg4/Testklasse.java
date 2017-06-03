5fb6584d475eda6fe88b049353d376d2020c3419
v <v@f>
2017-06-03 17:00:43 +0200

    b05a04: Vergessene Gates zu Tests hinzugefügt

--- Blatt05/Aufg4/Testklasse.java
+++ Blatt05/Aufg4/Testklasse.java
@@ -4,14 +4,15 @@ class Testklasse
 {
     public static void main(String args[])
     {
-        TestCase[] testCases = {new TestCase(new OrGate(), new boolean[] {false, true, true, true}),
+        TestCase[] testCases = {new TestCase(new AndGate(), new boolean[] {false, false, false, true}),
+                                new TestCase(new OrGate(), new boolean[] {false, true, true, true}),
                                 new TestCase(new NotGate(), new boolean[] {true, true, false, false}),
                                 new TestCase(new XorGate(), new boolean[] {false, true, true, false}),
+                                new TestCase(new NandGate(), new boolean[] {true, true, true, false}),
                                 new TestCase(new NorGate(), new boolean[] {true, false, false, false})};
 
         boolean allTestsPassed = true;
         for (TestCase tc: testCases) {
-
             if (!tc.runTest()) {
                 allTestsPassed = false;
             };
